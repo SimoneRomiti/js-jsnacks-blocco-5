e6f798abf8eb283cd7cff107f15fd7dd9a194444
Simone Romiti <sr1992ei@gmail.com>
2020-12-01 19:25:17 +0100

    Prove varie ES6

--- ProveEs6/js/script.js
+++ ProveEs6/js/script.js
@@ -47,7 +47,7 @@
 // }
 // console.log(i); //OK: var dentro un blocco if o in un ciclo ha comunque scope globale quindi è visibile anche fuori dal blocco dove è stato dichiarata
 
-// LET E CONST
+// // LET E CONST
 // var condizione = true;
 // if(condizione == true){
 //   let i = 5;
@@ -84,3 +84,90 @@
 //     <p>${paragrafo}</p>
 //   </div>
 //   `;
+// // FINE BACKTICK
+
+// // ARROW FUNCTION
+// // FUNZIONI CON NOME
+// function myFunction(){
+//   return 5 + 5;
+// }
+// var x = myFunction();
+// console.log("myFunction:", x);
+//
+// const myArrowFunction = () => 5 + 10; //Return implicito se scritto codice in una sola riga senza graffe
+// var y = myArrowFunction();
+// console.log("myArrowFunction", y);
+
+// // FUNZIONI ANONIME
+// document.getElementById("button").addEventListener("click",
+//   function(){
+//     alert("Ciao");
+//   }
+// );
+
+// document.getElementById("button").addEventListener("click",
+//   () => alert("Ciao a tutti")
+// );
+
+// const myArrowFunction2 = (num) => {
+//   var x = 10;
+//   var somma = num + x;
+//   return somma;
+// } // Se il codice è su più righe utilizzo le parentesi graffe ed il return non è più implicito
+// var result = myArrowFunction2(5);
+// console.log(result);
+
+// // ARROW FUNCTION THIS
+// document.getElementById("button").addEventListener("click",
+//   function(){
+//     console.log("funzione normale", this);
+//   }
+// );
+//
+// document.getElementById("button").addEventListener("click",
+//   () => {
+//     console.log("arrow function", this);
+//   }
+// );
+// // FINE ARROW FUNCTION
+
+// // SPREAD E REST
+// function myFunction(...myArguments){
+//   console.log(myArguments);
+// }
+// myFunction(1, 2, "Ciao", 57, "Simone", [58, 12, 23], {nome: "Simone", cognome: "Romiti"}); //...myArguments passato come parametro di una funzione significa che la funzione potrà avere un numero variabile di parametri da 1 a infinito
+//
+// var arr1 = [1, 2, 3];
+// var arr2 = [4, 5, 6];
+// var newArray = [arr1, arr2]; //Così ottengo un array bidimensionale che ha in posizione 0 l'array arr1 e in posizione 1 l'array arr2
+// var mergedArray = [...arr1,...arr2]; // Così ottengo un array che contiene gli elementi di arr1 e gli elementi di arr2
+// console.log(newArray);
+// console.log(mergedArray);
+//
+// var object = {
+//   nome: "Simone",
+//   cognome: "Romiti"
+// }
+// var copyObj = {
+//   ...object,
+//   eta: 28,
+// }
+// console.log(copyObj); //Posso mettere lo spread più il nome di un altro oggetto come elemento di un nuovo oggetto, in questo modo il nuovo oggetto prenderà tutte le coppie chiave: valore dell'oggetto inserito dopo lo spread più quelle che inserisco dopo
+// // FINE SPREAD E REST
+
+// // AGEVOLAZIONI OGGETTI
+// var nome = "nome";
+// var cognome = "cognome";
+// var object = {
+//   nome,
+//   cognome
+// };
+// console.log(object); //Se chiave e valore sono uguali basta scriverli semplicemente una volta
+//
+// var key = "eta";
+// var newObject = {
+//   nome: "Simone",
+//   cognome: "Romiti",
+//   [key]: 28
+// };
+// console.log(newObject); // Posso memorizzare una chiave in una variabile e richiamarla nell'oggetto mettendola tra parentesi quadre
